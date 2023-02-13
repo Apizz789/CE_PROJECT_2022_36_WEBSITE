f0af42ae59a68d6c945fa63d35fb30834690d1c2
Apirak Oulis <82326608+Apizz789@users.noreply.github.com>
2023-02-13 23:16:16 +0700

    upload pic fix bug

--- habiness-plus/src/component/Login.js
+++ habiness-plus/src/component/Login.js
@@ -4,15 +4,21 @@ function Login() {
     return (
         <div>
 
+          
             <div class="login_grid">
+            <div class="Pic_box">
+                <img
+                    src="https://raw.githubusercontent.com/Apizz789/CE_PROJECT_2022_36_WEBSITE/main/test001/public/picture/LOGO2_PNG%201-2.png"
+                    alt="logo" />
+            </div>
                 <form>
                     <div class="Un_box">
-                        <label class="label1">Username</label><br/>
+                        <label class="label1">Username</label><br />
                         <input class="username" type="text" id="Ausername" name="Ausername">
                         </input><br />
                     </div>
                     <div class="Pw_box">
-                        <label class="label2">Password</label><br/>
+                        <label class="label2">Password</label><br />
                         <input class="password" type="text" id="Apassword" name="Apassword">
                         </input><br />
                     </div>
