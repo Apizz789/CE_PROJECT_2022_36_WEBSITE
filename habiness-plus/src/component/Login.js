2d29c26688aaead5b4bfbc32fe22725d62f7ea05
Apirak Oulis <82326608+Apizz789@users.noreply.github.com>
2023-02-13 22:55:04 +0700

    update

--- habiness-plus/src/component/Login.js
+++ habiness-plus/src/component/Login.js
@@ -1,26 +1,28 @@
 import React from 'react'
-
+import './Login.css';
 function Login() {
     return (
         <div>
-            <form>
-                <div class="form-group">
-                    <label for="exampleInputEmail1">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-                </div>
-                <div class="form-group">
-                    <label for="exampleInputPassword1">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
-                </div>
-                <div class="form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
+
+            <div class="login_grid">
+                <form>
+                    <div class="Un_box">
+                        <label class="label1">Username</label><br/>
+                        <input class="username" type="text" id="Ausername" name="Ausername">
+                        </input><br />
+                    </div>
+                    <div class="Pw_box">
+                        <label class="label2">Password</label><br/>
+                        <input class="password" type="text" id="Apassword" name="Apassword">
+                        </input><br />
+                    </div>
+                    <input class="b1" type="submit" value="Submit" />
+                </form>
+            </div>
+
 
         </div>
+
 
     )
 }
