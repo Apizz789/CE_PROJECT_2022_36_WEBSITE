ceea8bde481448be6caf0c3fcd12f3672176796e
Apirak Oulis <82326608+Apizz789@users.noreply.github.com>
2023-02-22 00:11:56 +0700

    Anounment

--- habiness-plus/src/component/Announment.js
+++ habiness-plus/src/component/Announment.js
@@ -1,9 +1,17 @@
 import React, { useState } from 'react'
 import './Announment.css';
-import { DropdownButton, Dropdown, Col, Row, Modal } from 'react-bootstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { DropdownButton, Dropdown, Col, Row, Modal, Button } from 'react-bootstrap';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faTriangleExclamation } from '@fortawesome/free-solid-svg-icons'
+
+
+
+
 
 
 function Announment() {
@@ -15,6 +23,7 @@ function Announment() {
   };
 
   const [selectedDate, setSelectedDate] = useState(null);
+  const [showModal, setShowModal] = useState(false);
 
 
 
@@ -36,43 +45,33 @@ function Announment() {
 
           <Col class="waa">
             <Row> <label class="text_input2">Description</label></Row>
-            <Row><input type="text" class="text_box mb-2" placeholder="Enter your Description." /></Row>
+            <Row>
+              <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Enter your Description."></textarea>
+            </Row>
           </Col>
 
 
 
 
-        </Row><br />
+        </Row>
 
 
 
-        <Row>
+        <Row id="r2">
 
 
           <Col class="aa col-5">
-            <Row><input type="date" class="text_box mb-2" placeholder="Enter your Title." /></Row>
-            {/* <Col>
-              <DropdownButton title={selectedItem}>
-                <Dropdown.Item onClick={() => handleItemClick('Item 1')}>
-                  Item 1
-                </Dropdown.Item>
-                <Dropdown.Item onClick={() => handleItemClick('Item 2')}>
-                  Item 2
-                </Dropdown.Item>
-                <Dropdown.Item onClick={() => handleItemClick('Item 3')}>
-                  Item 3
-                </Dropdown.Item>
-              </DropdownButton>
-            </Col> */}
-
+            <Row> <label class="text_input3">Date</label></Row>
+            <Row><input type="date" class="date_box mb-2" placeholder="Enter your Title." /></Row>
           </Col>
 
           <Col class="aa col-2">
             <Col>
+              <Row> <label class="text_input4">Time</label></Row>
+              <Row><input type="time" class="time_box mb-2" placeholder="Enter your Title." /></Row>
+              <div>
 
-
-
-              <Row><input type="time" class="text_box mb-2" placeholder="Enter your Title." /></Row>
+              </div>
 
 
 
@@ -90,33 +89,27 @@ function Announment() {
 
 
         <Row>
-          <Col><button type="button" class="bt1 btn btn-primary">Send</button></Col>
-          {/* <!-- Button trigger modal --> */}
-          <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
-            Launch demo modal
-          </button>
+          <Col>
+            <Button variant="primary" data-target="#exampleModalCenter" onClick={() => setShowModal(true)}>
+              Send
+            </Button>
 
-          {/* <!-- Modal --> */}
-          <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered" role="document">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
-                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                  </button>
-                </div>
-                <div class="modal-body">
-                  ...
-                </div>
-                <div class="modal-footer">
-                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                  <button type="button" class="btn btn-primary">Save changes</button>
-                </div>
-              </div>
-            </div>
-          </div>
+            <Modal show={showModal} id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true" onHide={() => setShowModal(false)}>
+              <Modal.Header closeButton>
+                <Modal.Title><FontAwesomeIcon icon={faTriangleExclamation} /> Please be careful!</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>This Messeges will send to all user. </Modal.Body>
+              <Modal.Footer>
+
+                <Button variant="primary" onClick={() => console.log('Save changes')}>
+                  Send Messeges
+                </Button>
+
+              </Modal.Footer>
+            </Modal>
+          </Col>
         </Row>
+
 
 
 
